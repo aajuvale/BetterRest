b6a0bb5111bb5591133d94759cc195d5da20d6a7
aajuvale <ahmed.juvale@gmail.com>
2025-07-23 12:17:00 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ahmed Juvale on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
