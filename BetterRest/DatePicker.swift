292a353f3eb385a27834d611ce4f6abef4f9c1ca
aajuvale <ahmed.juvale@gmail.com>
2025-07-23 15:36:04 -0700

    feat: implemented formatted date time view

---
+++ BetterRest/DatePicker.swift
@@ -0,0 +1,27 @@
+//
+//  DatePicker.swift
+//  BetterRest
+//
+//  Created by Ahmed Juvale on 7/23/25.
+//
+
+import SwiftUI
+
+struct DatePickerView: View {
+    @State
+    private var wakeUp = Date.now
+
+    var body: some View {
+        DatePicker("Please enter a date: ", selection: $wakeUp, in: Date.now...)
+            .labelsHidden()
+    }
+
+    func exampleDates() {
+        let tomorrow = Date.now.addingTimeInterval(86400)
+        let range = Date.now...tomorrow
+    }
+}
+
+#Preview {
+    DatePickerView()
+}
